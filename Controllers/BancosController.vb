7d3b4eba6a4f2d82931a9dbbf489fff000b4effd
xseg18 <ximenagobern@gmail.com>
2023-07-14 09:46:39 -0600

    controlador de bancos

--- Controllers/BancosController.vb
+++ Controllers/BancosController.vb
@@ -1,12 +1,61 @@
-﻿Imports System.Web.Mvc
+﻿Imports System.Web.Http
+Imports System.Web.Mvc
+Imports SAPbobsCOM
+Imports WS_REST.RespuestaBanco
 
 Namespace Controllers
     Public Class BancosController
-        Inherits Controller
+        Inherits ApiController
 
         ' GET: Bancos
-        Function Index() As ActionResult
-            Return View()
+        Public Function GetValue(ByVal NombreBaseDatos As String) As RespuestaBanco
+            Dim oRespuesta As New RespuestaBanco
+            Dim oConexion As SAPbobsCOM.Company
+            Dim StrQuery As String = String.Empty
+            Dim lIntCodigoError As Integer = 0
+            Dim lStrTextoError As String = String.Empty
+            Dim ObjRespuesta As New Banco
+            Dim oRecordsetConsulta As SAPbobsCOM.Recordset
+
+            Try
+                oConexion = Funciones.FncSAPConexion(NombreBaseDatos, lIntCodigoError, lStrTextoError)
+
+                If lStrTextoError.Length = 0 Then
+
+                    oRecordsetConsulta = Nothing
+                    oRecordsetConsulta = oConexion.GetBusinessObject(BoObjectTypes.BoRecordset)
+                    StrQuery = " select ""BankCode"",""BankName"" from ODSC"
+                    oRecordsetConsulta.DoQuery(StrQuery)
+                    oRecordsetConsulta.MoveFirst()
+
+                    If oRecordsetConsulta.RecordCount = 0 Then
+                        oRespuesta.BlnEstado = False
+                        oRespuesta.StrMensaje = "NO SE ENCUENTRAN REGISTROS"
+                    Else
+                        oRespuesta.BlnEstado = True
+                        oRespuesta.StrMensaje = "OK"
+                        For i As Integer = 0 To oRecordsetConsulta.RecordCount - 1
+                            ObjRespuesta = New Banco
+                            ObjRespuesta.BankName = oRecordsetConsulta.Fields.Item("BankName").Value
+                            ObjRespuesta.BankCode = oRecordsetConsulta.Fields.Item("BankCode").Value
+                            oRespuesta.LstBancos.Add(ObjRespuesta)
+                            oRecordsetConsulta.MoveNext()
+                        Next
+                    End If
+                    oConexion.Disconnect()
+                    Return oRespuesta
+                Else
+                    oRespuesta = New RespuestaBanco
+                    oRespuesta.StrMensaje = "Error Conexion: " & lIntCodigoError & " - " & lStrTextoError
+                    oRespuesta.BlnEstado = False
+                    Return oRespuesta
+                End If
+            Catch ex As Exception
+                oRespuesta = New RespuestaBanco
+                oRespuesta.StrMensaje = "Error De Servidor: " & ex.Message
+                oRespuesta.BlnEstado = False
+                Return oRespuesta
+            End Try
         End Function
     End Class
 End Namespace
