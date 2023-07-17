d018f4ee1bc64faa2bba41e26620894ff54777c4
xseg18 <ximenagobern@gmail.com>
2023-07-17 11:54:48 -0600

    cuentas propias

---
+++ Controllers/CuentasPropiasController.vb
@@ -0,0 +1,59 @@
+﻿Imports System.Web.Http
+Imports System.Web.Mvc
+Imports SAPbobsCOM
+Imports WS_REST.RespuestaClientes
+Imports WS_REST.RespuestaCuentasPropias
+
+Namespace Controllers
+    Public Class CuentasPropiasController
+        Inherits ApiController
+        Public Function GetValue(ByVal NombreBaseDatos As String) As RespuestaCuentasPropias
+            Dim oRespuesta As New RespuestaCuentasPropias
+            Dim oConexion As SAPbobsCOM.Company
+            Dim StrQuery As String = String.Empty
+            Dim lIntCodigoError As Integer = 0
+            Dim lStrTextoError As String = String.Empty
+            Dim ObjRespuesta As New CuentaPropia
+            Dim oRecordsetConsulta As SAPbobsCOM.Recordset
+            Try
+                oConexion = Funciones.FncSAPConexion(NombreBaseDatos, lIntCodigoError, lStrTextoError)
+                If lStrTextoError.Length = 0 Then
+
+                    oRecordsetConsulta = Nothing
+                    oRecordsetConsulta = oConexion.GetBusinessObject(BoObjectTypes.BoRecordset)
+                    StrQuery = "select ""BankCode"". ""Account"" from DSC1"
+                    oRecordsetConsulta.DoQuery(StrQuery)
+                    oRecordsetConsulta.MoveFirst()
+                    oRespuesta = New RespuestaCuentasPropias
+                    If oRecordsetConsulta.RecordCount = 0 Then
+                        oRespuesta.BlnEstado = False
+                        oRespuesta.StrMensaje = "NO SE ENCUENTRAN REGISTROS"
+                    Else
+                        oRespuesta.BlnEstado = True
+                        oRespuesta.StrMensaje = "OK"
+                        For i As Integer = 0 To oRecordsetConsulta.RecordCount - 1
+                            ObjRespuesta = New CuentaPropia
+                            ObjRespuesta.BankCode = oRecordsetConsulta.Fields.Item("BankCode").Value
+                            ObjRespuesta.Account = oRecordsetConsulta.Fields.Item("Account").Value
+                            oRespuesta.LstCuentasPropias.Add(ObjRespuesta)
+                            oRecordsetConsulta.MoveNext()
+                        Next
+                    End If
+                    oConexion.Disconnect()
+                    Return oRespuesta
+                Else
+                    oRespuesta = New RespuestaCuentasPropias
+                    oRespuesta.StrMensaje = "Error Conexion: " & lIntCodigoError & " - " & lStrTextoError
+                    oRespuesta.BlnEstado = False
+                    Return oRespuesta
+                End If
+            Catch ex As Exception
+                oRespuesta = New RespuestaCuentasPropias
+                oRespuesta.StrMensaje = "Error De Servidor: " & ex.Message
+                oRespuesta.BlnEstado = False
+                Return oRespuesta
+            End Try
+        End Function
+
+    End Class
+End Namespace
---
+++ Models/RespuestaCuentasPropias.vb
@@ -0,0 +1,12 @@
+﻿
+
+Public Class RespuestaCuentasPropias
+    Public StrMensaje As String
+    Public BlnEstado As Boolean
+    Public LstCuentasPropias As New List(Of CuentaPropia)
+
+    Public Class CuentaPropia
+        Public BankCode As String
+        Public Account As String
+    End Class
+End Class
